8bf4ce6f878e58d1cc0f2c37f9b7480843e12daf
Pete McFarlane <peterjohnmcfarlane@gmail.com>
2021-07-08 18:13:22 +0100

    Improve typescript bolt-js handling

--- src/functions/slack/events/handler.ts
+++ src/functions/slack/events/handler.ts
@@ -1,24 +1,23 @@
 import "source-map-support/register";
-import { App, AwsLambdaReceiver } from "@slack/bolt";
+import {
+  App,
+  AwsLambdaReceiver,
+  BlockButtonAction,
+  BlockStaticSelectAction,
+} from "@slack/bolt";
 import {
   runInstance,
   startInstance,
   stopInstance,
   terminateInstance,
 } from "@libs/awsEc2";
-import {
-  AwsCallback,
-  AwsEvent,
-  AwsHandler,
-  AwsResponse,
-} from "@slack/bolt/dist/receivers/AwsLambdaReceiver";
+import { AwsHandler } from "@slack/bolt/dist/receivers/AwsLambdaReceiver";
 import { generateView } from "@libs/slackHelpers";
 
 const awsLambdaReceiver = new AwsLambdaReceiver({
   signingSecret: process.env.SLACK_SIGNING_SECRET,
 });
 
-// Initializes your app with your bot token and signing secret
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   receiver: awsLambdaReceiver,
@@ -43,7 +42,7 @@ app.shortcut("listInstances", async ({ shortcut, ack, client }) => {
   }
 });
 
-app.action("refreshView", async ({ ack, body, client }: any) => {
+app.action<BlockButtonAction>("refreshView", async ({ ack, body, client }) => {
   try {
     await ack();
     const view = await generateView();
@@ -56,51 +55,51 @@ app.action("refreshView", async ({ ack, body, client }: any) => {
   }
 });
 
-//SlackActionMiddlewareArgs<BlockStaticSelectAction>
-app.action("instance-action", async ({ ack, body, client, payload }: any) => {
-  try {
-    await ack();
-    const command = payload.selected_option.value;
-    let matches;
-    if ((matches = command.match(/terminate_(i-[\w]+)/))) {
-      await terminateInstance(matches[1]);
+app.action<BlockStaticSelectAction>(
+  "instance-action",
+  async ({ ack, body, client, payload }) => {
+    try {
+      await ack();
+      const command = payload.selected_option.value;
+      let matches;
+      if ((matches = command.match(/terminate_(i-[\w]+)/))) {
+        await terminateInstance(matches[1]);
+      }
+      if ((matches = command.match(/start_(i-[\w]+)/))) {
+        await startInstance(matches[1]);
+      }
+      if ((matches = command.match(/stop_(i-[\w]+)/))) {
+        await stopInstance(matches[1]);
+      }
+      const view = await generateView();
+      client.views.update({
+        view_id: body.view.id,
+        view,
+      });
+    } catch (error) {
+      console.error(error);
     }
-    if ((matches = command.match(/start_(i-[\w]+)/))) {
-      await startInstance(matches[1]);
-    }
-    if ((matches = command.match(/stop_(i-[\w]+)/))) {
-      await stopInstance(matches[1]);
-    }
-    const view = await generateView();
-    client.views.update({
-      view_id: body.view.id,
-      view,
-    });
-  } catch (error) {
-    console.error(error);
   }
-});
+);
 
-app.action("createInstance", async ({ ack, client, body }: any) => {
-  try {
-    await ack();
-    await runInstance();
-    const newView = await generateView();
-    client.views.update({
-      view_id: body.view.id,
-      // hash: body.view.hash,
-      view: newView,
-    });
-  } catch (error) {
-    console.error(error);
+app.action<BlockButtonAction>(
+  "createInstance",
+  async ({ ack, client, body }) => {
+    try {
+      await ack();
+      await runInstance();
+      const newView = await generateView();
+      client.views.update({
+        view_id: body.view.id,
+        view: newView,
+      });
+    } catch (error) {
+      console.error(error);
+    }
   }
-});
+);
 
-export const main = async (
-  event: AwsEvent,
-  context: any,
-  callback: AwsCallback
-): Promise<AwsResponse> => {
+export const main: AwsHandler = async (event, context, callback) => {
   // todo the library docs note that the start() should be made generic, see if I can get around to making a PR for this...
   const handler: AwsHandler | any = await app.start();
 
